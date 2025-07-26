5119982e1dffb21aad2c75d22229b55f67bdbc79
Tyan Kid <168115289+TyanKit@users.noreply.github.com>
2025-07-26 13:32:58 +0700

    Delete LogicGame/LogicGame.py

--- LogicGame/LogicGame.py
+++
@@ -1,9 +0,0 @@
-def determine_winner(move1, move2):
-    if move1 == move2:
-        return "draw"
-    elif (move1 == "rock" and move2 == "scissors") or \
-         (move1 == "scissors" and move2 == "paper") or \
-         (move1 == "paper" and move2 == "rock"):
-        return "player1"
-    else:
-        return "player2"
